a5cc45af6181eaf263e9fe9b9ec570c959757336
Spiegel <spiegel.e@finfort.ru>
2023-01-31 19:58:44 +0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Екатерина Шпигель on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
